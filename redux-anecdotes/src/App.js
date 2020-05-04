13a2d615f50b514ddb8b2155d9d2cefd836ea1fc
dietBlue <jeff.charters@gmail.com>
2020-05-04 15:36:42 -0400

    Finish part 6a

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,40 +1,14 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-
-import { voteForAnecdote, createAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const unorderedAnecdotes = useSelector(state => state)
-  const anecdotes = unorderedAnecdotes.sort((a, b) => (a.votes > b.votes) ? -1 : 1)
-  const dispatch = useDispatch()
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
 
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(voteForAnecdote(anecdote.id))}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote" /></div>
-        <button>create</button>
-      </form>
-    </div>
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div >
   )
 }
 
---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { voteForAnecdote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+    return (
+        <div>
+            <div>{anecdote.content}</div>
+            <div>
+                has {anecdote.votes}
+                <button onClick={handleClick}>vote</button>
+            </div>
+        </div>
+    )
+}
+
+const AnecdoteList = () => {
+    const dispatch = useDispatch()
+    const unorderedAnecdotes = useSelector(state => state)
+    const anecdotes = unorderedAnecdotes.sort((a, b) => (a.votes > b.votes) ? -1 : 1)
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            {anecdotes.map(anecdote =>
+                <Anecdote
+                    key={anecdote.id}
+                    anecdote={anecdote}
+                    handleClick={() => dispatch(voteForAnecdote(anecdote.id))}
+                />
+            )}
+        </div>
+    )
+}
+
+export default AnecdoteList
